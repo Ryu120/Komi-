5f83e8ad5baf712cc3e04c4482e9b90022a5321c
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-17 16:56:42 +0530

    Update cinfo.py

--- KomiXRyu/modules/cinfo.py
+++ KomiXRyu/modules/cinfo.py
@@ -1,10 +1,10 @@
-from zeldris import pbot as app
-from zeldris.modules.helper_funcs.section import section
+from KomiXRyu import pbot as app
+from KomiXRyu.modules.helper_funcs.section import section
 
 from telegram import ParseMode
 from pyrogram import filters
 from pyrogram.types import Message
-from zeldris import DEV_USERS
+from KomiXRyu import DEV_USERS
 
 
 async def get_chat_info(chat, already=False):
