ab36608375a6572cc4624b48c6b710894097f93e
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-18 05:20:05 +0530

    Update reporting.py

--- KomiXRyu/modules/reporting.py
+++ KomiXRyu/modules/reporting.py
@@ -1,19 +1,3 @@
-# ZeldrisRobot
-# Copyright (C) 2017-2019, Paul Larsen
-# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import html
 from typing import Optional
@@ -29,11 +13,11 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-from zeldris import dispatcher, LOGGER
-from zeldris.modules.helper_funcs.alternate import typing_action
-from zeldris.modules.helper_funcs.chat_status import user_not_admin, user_admin
-from zeldris.modules.log_channel import loggable
-from zeldris.modules.sql import reporting_sql as sql
+from KomiXRyu import dispatcher, LOGGER
+from KomiXRyu.modules.helper_funcs.alternate import typing_action
+from KomiXRyu.modules.helper_funcs.chat_status import user_not_admin, user_admin
+from KomiXRyu.modules.log_channel import loggable
+from KomiXRyu.modules.sql import reporting_sql as sql
 
 REPORT_GROUP = 5
 
