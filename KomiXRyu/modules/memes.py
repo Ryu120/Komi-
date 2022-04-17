603a58692ee72e3fcd3006b76b3b149e4146f77d
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-18 04:55:08 +0530

    Update memes.py

--- KomiXRyu/modules/memes.py
+++ KomiXRyu/modules/memes.py
@@ -1,19 +1,3 @@
-# ZeldrisRobot
-# Copyright (C) 2017-2019, Paul Larsen
-# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>..
 
 import random
 import re
@@ -25,15 +9,15 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters
 from telegram.utils.helpers import escape_markdown
 
-import zeldris.modules.helper_funcs.fun_strings as fun
-from zeldris import LOGGER, DEV_USERS, SUPPORT_USERS, dispatcher
-from zeldris.modules.disable import (
+import KomiXRyu.modules.helper_funcs.fun_strings as fun
+from KomiXRyu import LOGGER, DEV_USERS, SUPPORT_USERS, dispatcher
+from KomiXRyu.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from zeldris.modules.helper_funcs.alternate import typing_action
-from zeldris.modules.helper_funcs.extraction import extract_user
-from zeldris.modules.helper_funcs.filters import CustomFilters
+from KomiXRyu.modules.helper_funcs.alternate import typing_action
+from KomiXRyu.modules.helper_funcs.extraction import extract_user
+from KomiXRyu.modules.helper_funcs.filters import CustomFilters
 
 
 @typing_action
