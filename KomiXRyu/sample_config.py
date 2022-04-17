bd7715bbb062421f4735f3e4428e943bb5707c2f
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-17 16:09:58 +0530

    Update sample_config.py

--- KomiXRyu/sample_config.py
+++ KomiXRyu/sample_config.py
@@ -1,19 +1,3 @@
-# ZeldrisRobot
-# Copyright (C) 2017-2019, Paul Larsen
-# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import json
@@ -29,7 +13,7 @@ if not __name__.endswith("sample_config"):
     sys.exit(1)
 
 def get_user_list(config, key):
-    with open("{}/zeldris/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/KomiXRyu/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
