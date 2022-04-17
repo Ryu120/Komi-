456f56c7c25458da6d3ba7b42901890804f01f02
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-17 16:08:54 +0530

    Update mongo.py

--- KomiXRyu/mongo.py
+++ KomiXRyu/mongo.py
@@ -2,22 +2,22 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from zeldris import MONGO_DB_URI 
+from KomiXRyu import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from zeldris.conf import get_int_key, get_str_key
+from KomiXRyu.conf import get_int_key, get_str_key
 
 
 
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "Violet"
+MONGO_DB = "Komi"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
 db = motor[MONGO_DB]
-db = client["Violet"]
+db = client["Komi"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
