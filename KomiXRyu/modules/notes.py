2c8238511eb07c4e29dac2d2ce9154147c63789a
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-18 05:14:36 +0530

    Update notes.py

--- KomiXRyu/modules/notes.py
+++ KomiXRyu/modules/notes.py
@@ -1,19 +1,3 @@
-# ZeldrisRobot
-# Copyright (C) 2017-2019, Paul Larsen
-# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import ast
 import re
@@ -32,15 +16,15 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from telegram.utils.helpers import mention_html
 
-import zeldris.modules.sql.notes_sql as sql
-from zeldris import dispatcher, MESSAGE_DUMP, LOGGER
-from zeldris.modules.connection import connected
-from zeldris.modules.disable import DisableAbleCommandHandler
-from zeldris.modules.helper_funcs.alternate import typing_action
-from zeldris.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
-from zeldris.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from zeldris.modules.helper_funcs.msg_types import get_note_type
-from zeldris.modules.helper_funcs.string_handling import (
+import KomiXRyu.modules.sql.notes_sql as sql
+from KomiXRyu import dispatcher, MESSAGE_DUMP, LOGGER
+from KomiXRyu.modules.connection import connected
+from KomiXRyu.modules.disable import DisableAbleCommandHandler
+from KomiXRyu.modules.helper_funcs.alternate import typing_action
+from KomiXRyu.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from KomiXRyu.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from KomiXRyu.modules.helper_funcs.msg_types import get_note_type
+from KomiXRyu.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
