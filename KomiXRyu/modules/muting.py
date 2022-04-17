ca8510674471fcd224926dc66c677baf24eaf308
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-18 05:09:35 +0530

    Update muting.py

--- KomiXRyu/modules/muting.py
+++ KomiXRyu/modules/muting.py
@@ -1,19 +1,3 @@
-# ZeldrisRobot
-# Copyright (C) 2017-2019, Paul Larsen
-# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import html
 from typing import Optional
@@ -24,18 +8,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters
 from telegram.utils.helpers import mention_html
 
-from zeldris import dispatcher, LOGGER
-from zeldris.modules.helper_funcs.admin_rights import user_can_ban
-from zeldris.modules.helper_funcs.alternate import typing_action
-from zeldris.modules.helper_funcs.chat_status import (
+from KomiXRyu import dispatcher, LOGGER
+from KomiXRyu.modules.helper_funcs.admin_rights import user_can_ban
+from KomiXRyu.modules.helper_funcs.alternate import typing_action
+from KomiXRyu.modules.helper_funcs.chat_status import (
     bot_admin,
     user_admin,
     is_user_admin,
     can_restrict,
 )
-from zeldris.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from zeldris.modules.helper_funcs.string_handling import extract_time
-from zeldris.modules.log_channel import loggable
+from KomiXRyu.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from KomiXRyu.modules.helper_funcs.string_handling import extract_time
+from KomiXRyu.modules.log_channel import loggable
 
 
 @bot_admin
