9425a88dae3539ac492ab9937b7e4100cf036879
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-17 16:43:17 +0530

    Update antichan.py

--- KomiXRyu/modules/antichan.py
+++ KomiXRyu/modules/antichan.py
@@ -1,7 +1,7 @@
 """
 import asyncio
 
-from zeldris import pbot
+from KomiXRyu import pbot
 from pyrogram import Client, errors
 from pyrogram.raw import types, functions
 from pyrogram.raw.base import Update
