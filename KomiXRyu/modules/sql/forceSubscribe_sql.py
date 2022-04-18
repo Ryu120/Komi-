31a51f4eedebd1c00df40287ff6d123da4f6341c
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-18 12:16:24 +0530

    Update forceSubscribe_sql.py

--- KomiXRyu/modules/sql/forceSubscribe_sql.py
+++ KomiXRyu/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from zeldris.modules.sql import BASE, SESSION
+from KomiXRyu.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
