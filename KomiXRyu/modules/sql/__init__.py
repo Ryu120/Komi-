89e4d5e67229a02be00563cfdfbb0eb782ffacd0
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-17 23:38:04 -0700

    Komi

---
+++ KomiXRyu/modules/sql/__init__.py
@@ -0,0 +1,38 @@
+# ZeldrisRobot
+# Copyright (C) 2017-2019, Paul Larsen
+# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from zeldris import DB_URI, LOGGER
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    LOGGER.info("[PostgreSQL] Connecting to database......")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+try:
+    SESSION = start()
+except Exception as e:
+    LOGGER.exception(f"[PostgreSQL] Failed to connect due to {e}")
+    exit()
---
+++ KomiXRyu/modules/sql/blacklistusers_sql.py
@@ -0,0 +1,83 @@
+# ZeldrisRobot
+# Copyright (C) 2017-2019, Paul Larsen
+# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import threading
+
+from sqlalchemy import Column, String, UnicodeText
+
+from zeldris.modules.sql import BASE, SESSION
+
+
+class BlacklistUsers(BASE):
+    __tablename__ = "blacklistusers"
+    user_id = Column(String(14), primary_key=True)
+    reason = Column(UnicodeText)
+
+    def __init__(self, user_id, reason=None):
+        self.user_id = user_id
+        self.reason = reason
+
+
+BlacklistUsers.__table__.create(checkfirst=True)
+
+BLACKLIST_LOCK = threading.RLock()
+BLACKLIST_USERS = set()
+
+
+def blacklist_user(user_id, reason=None):
+    with BLACKLIST_LOCK:
+        user = SESSION.query(BlacklistUsers).get(str(user_id))
+        if not user:
+            user = BlacklistUsers(str(user_id), reason)
+        else:
+            user.reason = reason
+
+        SESSION.add(user)
+        SESSION.commit()
+        __load_blacklist_userid_list()
+
+
+def unblacklist_user(user_id):
+    with BLACKLIST_LOCK:
+        user = SESSION.query(BlacklistUsers).get(str(user_id))
+        if user:
+            SESSION.delete(user)
+
+        SESSION.commit()
+        __load_blacklist_userid_list()
+
+
+def get_reason(user_id):
+    user = SESSION.query(BlacklistUsers).get(str(user_id))
+    rep = user.reason if user else ""
+    SESSION.close()
+    return rep
+
+
+def is_user_blacklisted(user_id):
+    return user_id in BLACKLIST_USERS
+
+
+def __load_blacklist_userid_list():
+    global BLACKLIST_USERS
+    try:
+        BLACKLIST_USERS = {int(x.user_id) for x in SESSION.query(BlacklistUsers).all()}
+    finally:
+        SESSION.close()
+
+
+__load_blacklist_userid_list()
