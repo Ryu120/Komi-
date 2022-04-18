182a111fe5873182de6a34cb724f6cb1a4045207
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-04-18 09:59:52 +0530

    Update telegraph.py

--- KomiXRyu/modules/telegraph.py
+++ KomiXRyu/modules/telegraph.py
@@ -1,12 +1,12 @@
-from zeldris.events import register
-from zeldris import client as tbot
+from KomiXRyu.events import register
+from KomiXRyu import client as tbot
 TMP_DOWNLOAD_DIRECTORY = "tg-File/"
 from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-babe = "Voilet_Probot"
+babe = "KomiXryu_Bot"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=babe)
 auth_url = r["auth_url"]
