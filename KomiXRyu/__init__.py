17985284d66e893d234b1b3b0b93b8c6985dfacf
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-05-15 23:38:14 -0700

    BRUH

--- KomiXRyu/__init__.py
+++ KomiXRyu/__init__.py
@@ -1,51 +1,37 @@
-import asyncio
+
+
 import logging
 import os
 import sys
-import json
-import asyncio
 import time
+
 import spamwatch
 import telegram.ext as tg
-
-from inspect import getfullargspec
-from aiohttp import ClientSession
-from Python_ARQ import ARQ
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-from telethon.sessions import MemorySession
-from pyrogram.types import Message
 from pyrogram import Client, errors
-from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
-from pyrogram.types import Chat, User
-from ptbcontrib.postgres_persistence import PostgresPersistence
+from redis import StrictRedis
+from telethon import TelegramClient
+from telethon.sessions import MemorySession
 
 StartTime = time.time()
 
-def get_user_list(__init__, key):
-    with open("{}/KomiXRyu/{}".format(os.getcwd(), __init__), "r") as json_file:
-        return json.load(json_file)[key]
-
 # enable logging
-FORMAT = "[Komi] %(message)s"
 logging.basicConfig(
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler(),
+    ],
     level=logging.INFO,
-    format=FORMAT,
-    datefmt="[%X]",
 )
-logging.getLogger("pyrogram").setLevel(logging.INFO)
-logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
 
-LOGGER = logging.getLogger('[Komi]')
-LOGGER.info("Komi is starting. | Built by Ryu. | Licensed under GPLv3.")
-LOGGER.info("Yowaimo~ hehe just for fun lol.")
-LOGGER.info("Handled by: github.com/Ryu120 (t.me/GojoSenpai)")
+LOGGER = logging.getLogger(__name__)
 
-# if version < 3.9, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 9:
+LOGGER.info("[Komi] Starting Komi...")
+
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+        "[Komi] You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
     )
     sys.exit(1)
 
@@ -53,13 +39,12 @@ ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
     TOKEN = os.environ.get("TOKEN", None)
-
     try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+       OWNER_ID = int(os.environ.get("OWNER_ID", None))
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
-    JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
+    MESSAGE_DUMP = os.environ.get("MESSAGE_DUMP", None)
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
@@ -82,6 +67,23 @@ if ENV:
         TIGERS = {int(x) for x in os.environ.get("TIGERS", "").split()}
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
+
+    try:
+        WHITELIST_CHATS = {
+            int(x) for x in os.environ.get("WHITELIST_CHATS", "").split()
+        }
+    except ValueError:
+        raise Exception(
+            "[Komi] Your whitelisted chats list does not contain valid integers."
+        )
+    try:
+        BLACKLIST_CHATS = {
+            int(x) for x in os.environ.get("BLACKLIST_CHATS", "").split()
+        }
+    except ValueError:
+        raise Exception(
+            "[Komi] Your blacklisted chats list does not contain valid integers."
+        )
 
     WEBHOOK = bool(os.environ.get("WEBHOOK", False))
     URL = os.environ.get("URL", "")  # Does not contain token
@@ -110,180 +112,140 @@ if ENV:
     API_HASH = os.environ.get("API_HASH", None)
     SPAMWATCH = os.environ.get("SPAMWATCH_API", None)
     SPAMMERS = os.environ.get("SPAMMERS", None)
-  
-    
+
 else:
-    from KomiXRyu.config import Development as Config
+    from Komi.config import Development as Config
 
     TOKEN = Config.TOKEN
-
     try:
         OWNER_ID = int(Config.OWNER_ID)
     except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
+        raise Exception("[Komi] Your OWNER_ID variable is not a valid integer.")
 
-    JOIN_LOGGER = Config.JOIN_LOGGER
+    MESSAGE_DUMP = Config.MESSAGE_DUMP
     OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
+
     try:
-        DRAGONS = {int(x) for x in Config.DRAGONS or []}
         DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
     except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
+        raise Exception(
+            "[Komi] Your dev users list does not contain valid integers."
+        )
 
+    try:
+        SUPPORT_USERS = {int(x) for x in Config.SUPPORT_USERS or []}
+    except ValueError:
+        raise Exception(
+            "[Komi] Your support users list does not contain valid integers."
+        )
+
+    try:
+        WHITELIST_USERS = {int(x) for x in Config.WHITELIST_USERS or []}
+    except ValueError:
+        raise Exception(
+            "[Komi] Your whitelisted users list does not contain valid integers."
+        )
     try:
         DEMONS = {int(x) for x in Config.DEMONS or []}
     except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
+        raise Exception(
+            "[Komi] Your demons list does not contain valid integers."
+        )
     try:
-        WOLVES = {int(x) for x in Config.WOLVES or []}
+        WHITELIST_CHATS = {int(x) for x in Config.WHITELIST_CHATS or []}
     except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
+        raise Exception(
+            "[Komi] Your whitelisted chats list does not contain valid integers."
+        )
     try:
-        TIGERS = {int(x) for x in Config.TIGERS or []}
+        BLACKLIST_CHATS = {int(x) for x in Config.BLACKLIST_CHATS or []}
     except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+        raise Exception(
+            "[Komi] Your blacklisted users list does not contain valid integers."
+        )
 
-    EVENT_LOGS = Config.EVENT_LOGS
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
 
-    DB_URL = Config._DATABASE_URL
-    MONGO_DB_URI = Config.MONGO_DB_URI
-    ARQ_API_KEY = Config.ARQ_API_KEY
-    ARQ_API_URL = Config.ARQ_API_URL
+    DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    REDIS_URL = Config.REDIS_URL
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
     NO_LOAD = Config.NO_LOAD
-    ERROR_LOG = Config.ERROR_LOG
-    HEROKU_API_KEY = Config.HEROKU_API_KEY
-    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
     DEL_CMDS = Config.DEL_CMDS
     STRICT_GBAN = Config.STRICT_GBAN
     WORKERS = Config.WORKERS
-    REM_BG_API_KEY = Config.REM_BG_API_KEY
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
+    CUSTOM_CMD = Config.CUSTOM_CMD
+    API_WEATHER = Config.API_OPENWEATHER
     WALL_API = Config.WALL_API
     SUPPORT_CHAT = Config.SUPPORT_CHAT
-    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
-    SPAMWATCH_API = Config.SPAMWATCH_API
-    SESSION_STRING = Config.SESSION_STRING
-    INFOPIC = Config.INFOPIC
-    BOT_USERNAME = Config.BOT_USERNAME
-    STRING_SESSION = Config.STRING_SESSION
-    LASTFM_API_KEY = Config.LASTFM_API_KEY
-    CF_API_KEY = Config.CF_API_KEY
+    API_HASH = Config.API_HASH
+    API_ID = Config.API_ID
+    SPAMWATCH = Config.SPAMWATCH_API
+    SPAMMERS = Config.SPAMMERS
 
-    try:
-        BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-
-# If you forking dont remove this id, just add your id. LOL...
-
-DRAGONS.add(OWNER_ID)
+# Dont Remove This!!!
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(DEV_USERS)
-DEV_USERS.add(DEV_USERS)
+DEV_USERS.add(1791795037)
+DEV_USERS.add(2043468602)
+BOT_ID = 5092248582
+DEMONS.add(977805775)
+DEMONS.add(5288203109)
+SUPPORT_USERS.add(5128342147)
+SUPPORT_USERS.add(1835783426)
+SUPPORT_USERS.add(5147265129)
 
-if not SPAMWATCH_API:
-    sw = None
-    LOGGER.warning("SpamWatch API key missing! recheck your config")
+# Pass if SpamWatch token not set.
+if SPAMWATCH is None:
+    spamwtc = None
+    LOGGER.warning("[Komi] Invalid spamwatch api")
 else:
-    try:
-        sw = spamwatch.Client(SPAMWATCH_API)
-    except:
-        sw = None
-        LOGGER.warning("Can't connect to SpamWatch!")
+    spamwtc = spamwatch.Client(SPAMWATCH)
 
-from KomiXRyu.modules.sql import SESSION
-
-defaults = tg.Defaults(run_async=True)
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
-dispatcher = updater.dispatcher
-print("[INFO]: INITIALIZING AIOHTTP SESSION")
-aiohttpsession = ClientSession()
-# ARQ Client
-print("[INFO]: INITIALIZING ARQ CLIENT")
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
-
-ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
 try:
-    ubot2.start()
+    REDIS.ping()
+    LOGGER.info("[Komi] Your redis server is now alive!")
 except BaseException:
-    print("Userbot Error! Have you added a STRING_SESSION in deploying??")
-    sys.exit(1)
+    raise Exception("[Komi] Your redis server is not alive, please check again.")
+finally:
+    REDIS.ping()
+    LOGGER.info("[Komi] Your redis server is now alive!")
 
-pbot = Client(
-    ":memory:",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=TOKEN,
+# Telethon
+client = TelegramClient(MemorySession(), API_ID, API_HASH)
+updater = tg.Updater(
+    TOKEN,
     workers=min(32, os.cpu_count() + 4),
+    request_kwargs={"read_timeout": 10, "connect_timeout": 10},
 )
-apps = []
-apps.append(pbot)
-loop = asyncio.get_event_loop()
+dispatcher = updater.dispatcher
 
-async def get_entity(client, entity):
-    entity_client = client
-    if not isinstance(entity, Chat):
-        try:
-            entity = int(entity)
-        except ValueError:
-            pass
-        except TypeError:
-            entity = entity.id
-        try:
-            entity = await client.get_chat(entity)
-        except (PeerIdInvalid, ChannelInvalid):
-            for kp in apps:
-                if kp != client:
-                    try:
-                        entity = await kp.get_chat(entity)
-                    except (PeerIdInvalid, ChannelInvalid):
-                        pass
-                    else:
-                        entity_client = kp
-                        break
-            else:
-                entity = await kp.get_chat(entity)
-                entity_client = kp
-    return entity, entity_client
+pbot = Client("ErenPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot.start()
+    
 
-
-async def eor(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    return await func(**{k: v for k, v in kwargs.items() if k in spec})
-
-
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
+WHITELIST_USERS = list(WHITELIST_USERS)
+SUPPORT_USERS = list(SUPPORT_USERS)
 DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from EmikoRobot.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
+# pylint: disable=C0413
+from KomiXRyu.modules.helper_funcs.handlers import CustomCommandHandler
 
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+if CUSTOM_CMD and len(CUSTOM_CMD) >= 1:
+    tg.CommandHandler = CustomCommandHandler
+
+
+def spamfilters(text, user_id, chat_id):
+    # print("{} | {} | {}".format(text, user_id, chat_id))
+    if int(user_id) not in SPAMMERS:
+        return False
+
+    print("[Komi] This user is a spammer!")
+    return True
